732950f8162dd9f3203a36ab3a0eaab21fac1e0c
unknown <mututadaniel54@gmail.com>
2024-10-23 07:52:01 -0700

    updating components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
-// import { Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Experience from './components/Experience/Experience';
 import Footer from './components/Footer/Footer';
 import Menu from './components/Navbar/Menu';
-// import { Link}  from 'react-scroll'
-// import { Fragment } from 'react';
-// import { Link } from 'react-scroll';
-// import Footer from './componets/Footer/Footer';
-
-// import Navbar from './Components/Navbar/Navbar';
 
 function App() {
 
@@ -22,18 +15,6 @@ function App() {
 
   return (
     <div className="App">
-
-    {/* <Navbar/>
-   <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/about' element={<About/>} />
-   <Route path='/portfolio' element={<Portfolio/>}/>
-   <Route path='/contact' element={<Contact/>}/>
-   <Route path='/experince' element={<Experience/>}/>
-   </Routes>
-
- <Footer/> */}
-
 
 
      <Navbar />
