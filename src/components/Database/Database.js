50af04e143940631505a491b564630d5966c3959
unknown <mututadaniel54@gmail.com>
2025-01-21 10:32:08 +0200

    fixing database

--- src/components/Database/Database.js
+++ src/components/Database/Database.js
@@ -1,16 +1,28 @@
  
  const Database = [
+    
     {
         id:'1',
-        button:'View Website >',
-        name:'BOOTSTRAP',
-        description:'Highlighting my web development proficiency with HTML, CSS, and JavaScript, focusing on responsive layouts, clean code, and engaging user interactions.',
-        img:'bootstrape.png',
-        type:'personal projects',
-        link:'https://danielmututa.github.io/Bootstrape-portfolio/'
+        button:'CommingSoon>',
+        name:'OMINIVIEW',
+        description:"Introducing OMINIVIEW! I've built a cutting-edge platform for telecommunications, solar energy, and electronics repair using React, Node.js, Express, and MongoDB. Launching in 15 days! Stay tuned!",
+        img:'banner-img01.jpg',
+        type:'coming soon',
+        link:'https://ominiview.netlify.app'
     },
+
     {
         id:'2',
+        button:'View Website >',
+        name:'SPEEDBIKES',
+        description:'Rev up your ride! SPEEDBIKES e-commerce platform offering motorbikes, bicycles, genuine parts, and accessories, built with React, Node.js, Express, and MongoDB. Launching in 30 days!',
+        img:'speedbike.jpg',
+        type:'personal projects',
+        link:'https://speedbikes.netlify.app'
+    },
+
+    {
+        id:'3',
         button:'View Website >',
         name:'TIGZOZ',
         description:'Showcasing innovative web development techniques, TIGZOZ combines HTML, CSS, and JavaScript for a seamless blend of aesthetics and functionality.',
@@ -18,15 +30,7 @@
          type:'personal projects',
         link:'https://danielmututa.github.io/Tigzozo/'
     },
-    {
-        id:'3',
-        button:'View Website >',
-        name:'STYLISH PORTFOLIO',
-        description:'A showcase of sophistication and style, this portfolio project combines HTML, CSS, and JavaScript to create a captivating and professional online experience',
-        img:'stylish-portfolio.jpg',
-         type:'personal projects',
-        link:'https://danielmututa.github.io/Stylish-Portfolio/'
-    },
+   
     
     {
         id:'4',
@@ -37,24 +41,28 @@
         type:'collaboration',
         link:'https://kambuzuma-vanlife.netlify.app'
     },
+    
     {
         id:'5',
         button:'View Website >',
-        name:'SPEEDBIKES',
-        description:'Rev up your ride! SPEEDBIKES e-commerce platform offering motorbikes, bicycles, genuine parts, and accessories, built with React, Node.js, Express, and MongoDB. Launching in 30 days!',
-        img:'speedbike.jpg',
-        type:'personal projects',
-        link:'https://speedbikes.netlify.app'
+        name:'STYLISH PORTFOLIO',
+        description:'A showcase of sophistication and style, this portfolio project combines HTML, CSS, and JavaScript to create a captivating and professional online experience',
+        img:'stylish-portfolio.jpg',
+         type:'personal projects',
+        link:'https://danielmututa.github.io/Stylish-Portfolio/'
     },
+    
     {
         id:'6',
-        button:'CommingSoon>',
-        name:'OMINIVIEW',
-        description:"Introducing OMINIVIEW! I've built a cutting-edge platform for telecommunications, solar energy, and electronics repair using React, Node.js, Express, and MongoDB. Launching in 15 days! Stay tuned!",
-        img:'banner-img01.jpg',
-        type:'coming soon',
-        link:'https://ominiview.netlify.app'
+        button:'View Website >',
+        name:'BOOTSTRAP',
+        description:'Highlighting my web development proficiency with HTML, CSS, and JavaScript, focusing on responsive layouts, clean code, and engaging user interactions.',
+        img:'bootstrape.png',
+        type:'personal projects',
+        link:'https://danielmututa.github.io/Bootstrape-portfolio/'
     },
+
+   
 ];
 
 
